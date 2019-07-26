be78b2afdbde7cd0c34f367553e844e7a91ea263
Mathew Rigdon <mrigdon44@gmail.com>
2019-07-25 19:25:48 -0500

    model filter and color filter files

---
+++ Color_Filter.rb
@@ -0,0 +1,46 @@
+
+require_relative "72219"
+puts "\n"
+puts " Car Dealer Manager (CDM) ".center(100,'#')
+require 'readline'
+filtered_model=$filtered_model
+
+
+def input(prompt="", newline=false)
+  prompt += "\n" if newline
+  Readline.readline(prompt, true).squeeze(" ").strip
+end
+
+puts "\n*** Please Select one of the Following... *** \n"
+
+i = 1
+matches = 0
+colors = Array.new
+filtered_model.each do |color|
+  color = color[1]
+  colors_array = colors.include?(color)
+    if (colors_array != true)
+      colors.push(color)
+      puts "Enter #{i} for #{color}. \n"
+      matches += 1
+      i += 1
+    end
+end
+
+userinput1 = input
+
+userinput = colors.values_at(userinput1.to_i - 1)
+
+filtered_color = Array.new
+
+filtered_model.each do |car|
+   model = car[0]
+   color = car[1]
+   price = car[2]
+       #puts models.values_at(input.to_i)
+      if color == userinput[0]
+        filtered_color.push(model, color, price)
+        puts "The car is a #{color} #{model.capitalize} priced at #{price}\n"
+      end
+end
+puts matches
--- Model_Filter.rb
+++ Model_Filter.rb
@@ -27,18 +27,16 @@ cars = [
   ["fiesta", "black", "25K"],
   ["fiesta", "white", "25K"]
 ]
-​
  puts "\n"
  puts " Car Dealer Manager (CDM) ".center(100,'#')
  require 'readline'
-​
  def input(prompt="", newline=false)
    prompt += "\n" if newline
    Readline.readline(prompt, true).squeeze(" ").strip
  end
- ​
+
  puts "\n*** Please Select one of the Following... *** \n"
- ​
+
  i = 1
  matches = 0
  models = Array.new
@@ -52,28 +50,25 @@ cars = [
        i += 1
      end
  end
+
  userinput1 = input
- ​
+
  userinput = models.values_at(userinput1.to_i - 1)
+
+ filtered_model = Array.new
 
  cars.each do |car|
     model = car[0]
     color = car[1]
     price = car[2]
-#       #puts models.values_at(input.to_i)
+       #puts models.values_at(input.to_i)
        if model == userinput[0]
+         filtered_model.push([model, color, price])
          puts "The car is a #{color} #{model.capitalize} priced at #{price}\n"
        end
  end
-puts matches
-​
-​
- # if userinput1 != (1..matches)
- #    puts "*ERROR Please Enter..."
- #    (1.. matches - 1).each do
- #       |n| print "#{n}, "
- #     end
- #     puts "or #{matches}"
- #     #puts "\n"
- # end
-Collapse
+$filtered_model=filtered_model
+
+if __FILE__ == $0
+  print filtered_model
+end
