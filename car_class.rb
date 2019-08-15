a4b75409adf340473fb93258a78b55a0a00b5e79
Joshua Swartzell <joshuas193@live.com>
2019-08-15 17:36:30 -0500

    update 8.15.2019

--- car_class.rb
+++ car_class.rb
@@ -1,39 +1,12 @@
-require_relative 'database'
-cars = $cars
-
-
 class Car
-  attr_accessor :id, :make, :model, :color, :price, :type
 
-  def initialize (id, make, model, color, price, type)
-    @id = id
-    @make = make
-    @model = model
+  attr_accessor :make, :model, :color, :price
+
+  def initialize(make, model, color, price)
+    @make = make.capitalize
+    @model = model.capitalize
     @color = color
     @price = price
-    @type = type
   end
+
 end
-
-# cars.each do |car|
-#   make = car[0]
-#   model = car[1]
-#   color = car[2]
-#   price = car[3]
-#   type = car[4]
-#   car1 = Car.new (make, model, color, price, type)
-#   # models.push (model)
-#   # print models
-#   # car.new(car.make, car.model, car.color, car.price, car.type)
-# end
-
-
-
-
-
-fleet = Array.new
-ObjectSpace.each_object(Car) do |car|
-  fleet.push([car.id, car.model, car.color, car.price])
-end
-
-print fleet
