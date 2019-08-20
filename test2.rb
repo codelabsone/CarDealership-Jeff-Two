ba13d21857197b22c96fd73496b7b7f549bade9c
Joshua Swartzell <joshuas193@live.com>
2019-08-19 19:48:55 -0500

    Remove clutter

--- test2.rb
+++
@@ -1,8 +0,0 @@
-cars = Array.new
-color = Array.new
-pricerange = Array.new
-cars = ["escape", "focuse", "fiesta"]
-color = ["red", "black", "white"]
-pricerange = ["15K", "20K", "25K"]
-
-puts cars[0], color, pricerange
