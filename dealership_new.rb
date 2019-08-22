40716058249af369636663782bb2fb857c6e0940
Joshua Swartzell <joshuas193@live.com>
2019-08-22 17:38:59 -0500

    It Works!

--- dealership_new.rb
+++ dealership_new.rb
@@ -8,51 +8,66 @@ class Dealership
     @title = title
   end
 
-  def user_interface
-
-    begin
-
-        db = SQLite3::Database.open "cars_data.db"
-        db.results_as_hash = true
-        ary = db.execute "SELECT * FROM Cars"
-        puts "\sWhat model of car would you like?"
-        puts "\n"
-        models = []
-        colors = []
-        ary.each do |row|
-          models.push(row['Model'])
-        end
-        models.uniq.each do |model|
-          puts model.capitalize
-        end
-        answer1 = gets.chomp.downcase
-
-        ary.each do |row|
-          colors.push(row['Color'])
-        end
-        puts "\nWhat color would you like?"
-        colors.uniq.each do |color|
-          puts color.capitalize
-        end
-        answer2 = gets.chomp.downcase
-        ary.each do |row|
-          if row['Model'] == answer1
-            if row['Color'] == answer2
-              printf "We have a %s %s %s for $%s\n",row['Color'], row['Make'].capitalize,
-              row['Model'].capitalize, row['Price']
-            end
-          end
-        end
-
-    rescue SQLite3::Exception => e
-
-        puts "Exception occurred"
-        puts e
-
-    ensure
-        db.close if db
-    end
-
+  def greeting
+    puts "We have a great selection to choose from."
+    puts "\n"
   end
 
+  def user_interface
+    begin
+      db = SQLite3::Database.open "cars_data.db"
+      db.results_as_hash = true
+      ary = db.execute "SELECT * FROM Cars"
+      models = []
+      ary.each do |row|
+        models.push(row['Model'])
+      end
+      puts "We currently have #{models.size} cars and trucks to choose from."
+      puts "\nPlease select the model you would like:"
+      puts "\n"
+      models.uniq.each do |model|
+        puts model.capitalize
+      end
+    rescue SQLite3::Exception => e
+      puts "Exception occurred"
+      puts e
+    ensure
+      db.close if db
+    end
+
+    begin
+      db = SQLite3::Database.open "cars_data.db"
+      puts "\n"
+      answer1 = gets.chomp.downcase
+      puts "\n"
+      model = answer1
+      stm = db.prepare "SELECT Color FROM Cars WHERE Model = ?"
+      stm.bind_param 1, model
+      ary2 = stm.execute
+      ary2 = ary2.sort
+      puts "Please select from the colors below:"
+      puts "\n"
+      ary2.uniq.each do |color|
+        puts color
+      end
+    rescue SQLite3::Exception => e
+      puts "Exception occurred"
+      puts e
+    ensure
+      stm.close if stm
+      db.close if db
+    end
+
+      puts "\n"
+      answer2 = gets.chomp.downcase
+      puts "\n"
+    ary.each do |row|
+      if row['Model'] == answer1
+        if row['Color'] == answer2
+          printf "We have a %s %s %s for $%s\n",row['Color'].capitalize, row['Make'].capitalize,
+          row['Model'].capitalize, row['Price']
+        end
+      end
+    end
+  end
 end
--- interact.rb
+++ interact.rb
@@ -1,7 +1,8 @@
 require_relative 'dealership_new'
 
 dealer = Dealership.new(" Jeff's Car Dealer 2 ")
-puts "#{dealer.title}".center(100, '*')
-puts "\n\sWelcome to #{dealer.title.strip}"
+puts "#{dealer.title}".center(120, '=')
+puts "\nWelcome to #{dealer.title.strip}!"
 
+dealer.greeting
 dealer.user_interface
